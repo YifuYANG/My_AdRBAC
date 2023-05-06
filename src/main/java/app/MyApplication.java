60e34c69b21b3fa3a0694af20c7e039632562e0e
yifuyang <yifu.yang@ucdconnect.ie>
2023-05-06 20:32:31 +0100

    policy set updated, change to AuthzForce Core engine, able to load policies to the engine and tested.

--- src/main/java/app/MyApplication.java
+++ src/main/java/app/MyApplication.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 //@SpringBootApplication
 public class MyApplication {
     public static void main(String[] args) throws IOException {
-//        My_PDP p = new My_PDP();
-//        p.nihao();
         //SpringApplication.run(MyApplication.class, args);
         My_PDP p = new My_PDP();
         p.hello();
--- src/main/java/app/xacml/pdp/My_PDP.java
+++ src/main/java/app/xacml/pdp/My_PDP.java
@@ -16,14 +16,18 @@ import java.util.Optional;
 import static org.ow2.authzforce.xacml.identifiers.XacmlAttributeCategory.*;
 
 public class My_PDP {
+    private final PdpEngineConfiguration pdpEngineConf;
+    public My_PDP() throws IOException {
+        pdpEngineConf = PdpEngineConfiguration.getInstance("src/main/resources/PDP.xml");
+    }
+    //final PdpEngineConfiguration pdpEngineConf = PdpEngineConfiguration.getInstance("src/main/resources/PDP.xml");
     public void hello() throws IOException {
-        final PdpEngineConfiguration pdpEngineConf = PdpEngineConfiguration.getInstance("src/main/resources/PDP.xml");
         final BasePdpEngine pdp = new BasePdpEngine(pdpEngineConf);
         final DecisionRequestBuilder<?> requestBuilder = pdp.newRequestBuilder(-1, -1);
 
 
         final AttributeFqn subjectIdAttributeId = AttributeFqns.newInstance(XACML_1_0_ACCESS_SUBJECT.value(), Optional.empty(), XacmlAttributeId.XACML_1_0_SUBJECT_ID.value());
-        final AttributeBag<?> subjectIdAttributeValues = Bags.singletonAttributeBag(StandardDatatypes.STRING, new StringValue("doctor"));
+        final AttributeBag<?> subjectIdAttributeValues = Bags.singletonAttributeBag(StandardDatatypes.STRING, new StringValue("admin"));
         requestBuilder.putNamedAttributeIfAbsent(subjectIdAttributeId, subjectIdAttributeValues);
 
         final AttributeFqn resourceIdAttributeId = AttributeFqns.newInstance(XACML_3_0_ENVIRONMENT.value(), Optional.empty(), "urn:oasis:names:tc:xacml:1.0:environment:environment-id");
@@ -37,11 +41,13 @@ public class My_PDP {
         final DecisionRequest request = requestBuilder.build(false);
         // Evaluate the request
         final DecisionResult result = pdp.evaluate(request);
-        if(result.getDecision() == DecisionType.PERMIT) {
-            System.out.println("=================");
-        } else {
-            System.out.println("XXXXXXXXXXXXXXXXXX");
-        }
 
+        System.out.println("==================================================");
+        if(result.getDecision() == DecisionType.PERMIT) {
+            System.out.println("Permit!!!!");
+        } else {
+            System.out.println("Deny!!!!!");
+        }
+        System.out.println("==================================================");
     }
 }
